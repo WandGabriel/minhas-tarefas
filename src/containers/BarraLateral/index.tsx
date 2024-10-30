dee374c555473f97054a16ba9476a6b87de64503
Wanderson Gabriel <gabriel_wanderson@hotmail.com>
2024-10-30 20:21:55 -0300

    Configuração Inicial + Criação de Template (Sidebar + Seção Main) e estilização.

---
+++ src/containers/BarraLateral/index.tsx
@@ -0,0 +1,19 @@
+import FiltroCard from '../../components/FiltroCard'
+
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <S.Campo type="text" placeholder="Buscar" />
+    <S.Filtros>
+      <FiltroCard legenda="pendentes" contador={1} />
+      <FiltroCard legenda="concluidas" contador={2} />
+      <FiltroCard legenda="urgentes" contador={3} />
+      <FiltroCard legenda="importantes" contador={4} />
+      <FiltroCard legenda="normal" contador={5} />
+      <FiltroCard legenda="todas" contador={10} />
+    </S.Filtros>
+  </S.Aside>
+)
+
+export default BarraLateral
